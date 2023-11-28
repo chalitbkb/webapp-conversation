828c2194d15340335fbc6dfeafebc692f2ba251e
chalitguza <32437546+chalitguza@users.noreply.github.com>
2023-11-29 02:05:46 +0700

    Add files via upload

--- config/index.ts
+++ config/index.ts
@@ -3,11 +3,11 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
-  description: '',
-  copyright: '',
+  title: 'Tradingview Chatbot',
+  description: 'สวัสดี (กลุ่มผู้ใช้ Tradingview Thailand) Chatbot นี้ผ่านการฝึกข้อมูลล่าสุดเกี่ยวกับ Tradingview เพื่อสนับสนุนคำถามและการแก้ปัญหาที่คุณอาจมีในการใช้งาน Tradingview by hapharmonic',
+  copyright: 'HAP',
   privacy_policy: '',
-  default_language: 'zh-Hans',
+  default_language: 'th-Hans',
 }
 
 export const isShowPrompt = false
